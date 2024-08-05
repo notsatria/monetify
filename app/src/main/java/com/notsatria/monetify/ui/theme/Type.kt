fbfdbbe0d227889f9784f07ec0e474dac0b41b16
notsatria <buanasatriaa@gmail.com>
2024-08-05 20:43:49 +0700

    theme: setup Type.kt

--- app/src/main/java/com/notsatria/monetify/ui/theme/Type.kt
+++ app/src/main/java/com/notsatria/monetify/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.notsatria.monetify.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.notsatria.monetify.R
+
+val interFamily = FontFamily(
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = interFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
