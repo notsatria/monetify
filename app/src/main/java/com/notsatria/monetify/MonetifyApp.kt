db308a854abb3ebf01f9fe00228c745c59ba30bb
notsatria <buanasatriaa@gmail.com>
2024-08-05 20:37:29 +0700

    theme: setup Color.kt

---
+++ app/src/main/java/com/notsatria/monetify/MonetifyApp.kt
@@ -0,0 +1,9 @@
+package com.notsatria.monetify
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MonetifyApp(modifier: Modifier = Modifier) {
+
+}
--- app/src/main/java/com/notsatria/monetify/ui/theme/Color.kt
+++ app/src/main/java/com/notsatria/monetify/ui/theme/Color.kt
@@ -1,6 +1,8 @@
 package com.notsatria.monetify.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.notsatria.monetify.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +11,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Indigo = Color(0xFF6574D3)
+val Pink = Color(0xFFF0A7D2)
+val Blue = Color(0xFF66C2DB)
+val Orange = Color(0xFFF5D8A0)
+val Navy = Color(0xFF1A2130)
+val Red = Color(0xFFDB6565)
+val LightGrey = Color(0xFFF4F4F4)
